3a740951b8bd381e3d13c26ae3ae7725fbb14efa
sahrulrizal <sahrulrizal22@gmail.com>
2016-05-17 02:05:54 +0700

    Baru

--- application/views/main.php
+++ application/views/main.php
@@ -4,9 +4,27 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>APLIKASI TESTING</title>
-	<link rel="stylesheet" href="">
+	<!-- META -->
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
 </head>
 <body>
-	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="panel panel-success">
+					<div class="panel-heading">Testing</div>
+					<div class="panel-body">
+						<p>DA Number Option</p>
+						<input type="radio" id="radio" name="pre-printed"> Pre-Printed <input type="radio" id="radio" name="manual"> Manual
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>										
+	<!-- Script -->
+	<script src="<?= base_url('assets/js/jquery.min.js'); ?>"></script>
+	<script src="<?= base_url('assets/js/bootstrap.min.js'); ?>"></script>
 </body>
 </html>
